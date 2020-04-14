367a59f86fc8eb5cf8160e70950542672e1fad2e
Francesco Ceccon <francesco@ceccon.me>
2020-04-14 20:08:47 +0100

    refactor: split saon in different assemblies for json and query

---
+++ src/Saon.Json/Parser.fs
@@ -0,0 +1,129 @@
+namespace Saon.Json
+
+open System.Text.Json
+open Saon
+
+/// A `JsonParser` parses `JsonElement`s.
+type JsonParser<'T> = Parser<'T, JsonElement>
+
+
+type JsonObjectParserBuilder() =
+    member __.Run (parser : JsonParser<'T>) =
+        let validate (element : JsonElement) : ParserResult<'T> =
+            let result, _ = parser element
+            result
+        validate
+
+    member __.Bind(parser, f) = Parser.bind f parser
+    member __.Zero () = Parser.init ()
+    member __.Return (value : 'R) : JsonParser<'R> = Parser.init value
+
+
+[<AutoOpen>]
+module ParserBuilder =
+    let jsonObjectParser = JsonObjectParserBuilder()
+
+    let createRootParser (parser : JsonElement -> ParserResult<'T>) =
+        let parse (document : JsonDocument) =
+            parser document.RootElement
+        parse
+
+
+[<RequireQualifiedAccess>]
+module Json =
+    open System
+
+    let createDocumentParser (parser : JsonElement -> ParserResult<'T>) =
+        let parse (document : JsonDocument) =
+            parser document.RootElement
+        parse
+
+    /// Return `ParsingFailed` if `f` raises an exception.
+    let internal catchFail propName (f : unit -> 'T) =
+        try
+            f () |> Success
+        with ex ->
+            ParsingFailed (Some propName, ex.Message)
+
+    let property (propName : string) (func : Transformer<JsonElement, 'T>) (element : JsonElement) =
+        let found, propElement = element.TryGetProperty propName
+        if found then
+            let result = func propName propElement
+            result, element
+        else
+            let msg = sprintf "The required property '%s' is missing" propName
+            ParserResult.validationFail "missingProperty" propName msg, element
+
+    let optionalProperty (propName : string) (func : Transformer<JsonElement, 'T>) (element : JsonElement) =
+        let found, propElement = element.TryGetProperty propName
+        if found then
+            let result = func propName propElement |> ParserResult.map Some
+            result, element
+        else
+            ParserResult.success None, element
+
+    /// Get the `element` string value.
+    let string propName (element : JsonElement) =
+        catchFail propName (fun _ -> element.GetString())
+
+    /// Get the `element` int64 value.
+    let int64 propName (element : JsonElement) =
+        catchFail propName (fun _ -> element.GetInt64())
+
+    /// Get the `element` bool value.
+    let bool propName (element : JsonElement) =
+        catchFail propName (fun _ -> element.GetBoolean())
+
+    let object (inner : JsonElement -> ParserResult<'T>) propName (element : JsonElement) =
+        let addNestLevel field =
+            if String.IsNullOrEmpty(propName) then
+                field
+            elif String.IsNullOrEmpty(field) then
+                propName
+            else
+                sprintf "%s.%s" propName field
+        match inner element with
+        | Success v -> Success v
+        | ParsingFailed (None, msg) -> ParsingFailed (None, msg)
+        | ParsingFailed (Some field, msg) ->
+            let field = addNestLevel field
+            ParsingFailed (Some field, msg)
+        | ValidationFailed failMap ->
+            failMap
+            |> Map.fold (fun m k v -> Map.add (addNestLevel k) v m) Map.empty
+            |> ValidationFailed
+
+    /// Apply the `inner` parser to each children of the json array `element`.
+    let list (inner : Transformer<JsonElement, 'T>) propName (element : JsonElement) =
+        let folder (state, idx) el =
+            let nextIdx = idx + 1
+            let addNestLevel k =
+                if String.IsNullOrEmpty(k) then
+                    sprintf "[%d]" idx
+                else
+                    sprintf "[%d].%s" idx k
+
+            match state with
+            | Success xs ->
+                match inner propName el with
+                | Success x -> Success (x :: xs), nextIdx
+                | ValidationFailed failMap ->
+                    let failMap' =
+                        failMap
+                        |> Map.fold (fun m k v -> Map.add (addNestLevel k) v m) Map.empty
+                    ValidationFailed failMap', nextIdx
+                | ParsingFailed (field, msg) -> ParsingFailed (field, msg), nextIdx
+            | ValidationFailed failMap ->
+                match inner propName el with
+                | Success _ -> ValidationFailed failMap, nextIdx
+                | ValidationFailed failMap' ->
+                    let failMap'' =
+                        Map.fold (fun s k v -> Map.add (addNestLevel k) v s) failMap failMap'
+                    ValidationFailed failMap'', nextIdx
+                | ParsingFailed (field, msg) -> ParsingFailed (field, msg), nextIdx
+            | ParsingFailed (field, msg) -> ParsingFailed (field, msg), nextIdx
+
+        let result = element.EnumerateArray() |> Seq.fold folder (Success [], 0)
+        match result with
+        | Success xs, _ -> List.rev xs |> Success
+        | fail, _ -> fail
---
+++ src/Saon.Query/Library.fs
@@ -0,0 +1,5 @@
+﻿namespace Saon.Query
+
+module Say =
+    let hello name =
+        printfn "Hello %s" name
